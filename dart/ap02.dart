1b656aa1d850aa6cdc8ba61002f6e760185d233a
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-24 10:50:27 -0300

    ap02(dart): pedra papel tesoura
    - propoe o exercicio apenas com comentarios
    
    - retomar implementando com funções

---
+++ dart/ap02.dart
@@ -0,0 +1,55 @@
+
+
+// try{
+
+//   String s = null;
+//   s.toLowerCase();
+// }
+// catch(NullPointerException e){
+
+// }
+
+
+// import 'dart:io';
+// void main(){
+//   var nome = stdin.readLineSync();
+//   stdout.writeln(nome);
+//   String? m = nome?.toUpperCase();
+//   // String idadeTextual = stdin.readLineSync()!;
+//   // var idade = int.parse(idadeTextual);
+//   // var idadeTextual = stdin.readLineSync();
+//   // if(idadeTextual != null)
+//   //   int idade = int.parse(idadeTextual);
+// }
+
+// enum DiaDaSemana{
+//   segundaFeira,
+//   tercaFeira
+// }
+// void main(){
+//   DiaDaSemana dia = DiaDaSemana.segundaFeira;
+//   switch(dia){
+//     case DiaDaSemana.segundaFeira:
+//       print("AAA");
+//     default:
+//       print("Tudo bem");
+//   }
+  // print(DiaDaSemana.values);
+  // const dia = 1;
+  // switch(dia){
+  //   case 1:
+  //     print("AAAA");
+  //   default:
+  //     print("Tudo bem");
+  // }
+}
+
+// void main(){
+//   //for each
+//   const nomes = ["Pedro", "Maria", "João"];
+
+
+//   for (final nome in nomes){
+//     print(nome);
+//   }
+// }
---
+++ dart/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
@@ -0,0 +1,25 @@
+//importar a biblioteca para entrada/saída
+
+//importar a biblioteca para gerar valores aleatórios
+
+//definir um enum para representar as opções pedra, papel ... sair
+void jogo(){
+
+  //fazer o loop do jogo com do/while, ele termina quando o usuário digitar 4
+
+    //null loop do/while interno, fazer
+    //exibição do menu de opções
+    //capturar a opção do usuário, usando esse do/while para validá-la
+
+    //sortear a opção do computador
+
+   //mapear a opção do usuário, de int para enum
+
+   //mapear a opção do computador, de int para enum
+
+   //mostrar as opções no seguinte formato, com interpolação
+   //JOGADOR(pedra) vs (tesoura)COMPUTADOR
+
+   //exibir quem ganhou ou se houve empate
+
+}
